51b729ea281d93fc828055581afab8374cd810a1
黄文腾_CN <69565303+cljhwt@users.noreply.github.com>
2024-03-02 04:42:24 +0000

    居中

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,25 +1,7 @@
-/**
- * Home component
- *
- * The section at the top of the page to display image of your
- * choice, name and title that describes your career focus.
- */
-
 import React from "react";
 import arrowSvg from "../images/down-arrow.svg";
 import PropTypes from "prop-types";
 
-/**
- * Home background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
- * represents what you see in that image.
- *
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
 import image from "../images/微信图片_20240210114747.jpg";
 
 const videoSrc = "https://cdn.openai.com/tmp/s/paper-planes.mp4";
@@ -28,15 +10,15 @@ const imageAltText = "首页头图，保持更新";
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
-      <video className="background" autoPlay loop muted>
+      <video className="background" autoPlay loop muted style={{ display: "block", margin: "0 auto" }}>
         <source src={videoSrc} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div style={{ position: "absolute", top: "15rem", left: "2rem", color: "white", fontFamily: "Microsoft YaHei, Arial, sans-serif", textAlign: "center" }}>
+      <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", color: "white", fontFamily: "Microsoft YaHei, Arial, sans-serif", textAlign: "center" }}>
         <h1>{name}</h1>
         <h2>{title}</h2>
       </div>
-      <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
+      <div style={{ position: "absolute", bottom: "3rem", left: "50%", transform: "translateX(-50%)" }}>
         <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
       </div>
     </section>
